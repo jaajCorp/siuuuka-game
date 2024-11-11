39fb7d9d66cc8da9b08130888f048f5d965b95ef
GaspardCulis <gaspard@gasdev.fr>
2024-11-11 19:27:02 +0100

    feat: Added new politics

--- scripts/TextureRegistry.gd
+++ scripts/TextureRegistry.gd
@@ -3,12 +3,15 @@ extends Node
 const POLITICIANS := [
 	preload("res://assets/img/french_politicians/hollande.png"),
 	preload("res://assets/img/french_politicians/sarko.png"),
+	preload("res://assets/img/french_politicians/fillon.png"),
 	preload("res://assets/img/french_politicians/bardella.png"),
-	preload("res://assets/img/french_politicians/zemmour.png"),
 	preload("res://assets/img/french_politicians/marine.png"),
+	preload("res://assets/img/french_politicians/zemmour.png"),
 	preload("res://assets/img/french_politicians/macron.png"),
-	preload("res://assets/img/french_politicians/jean-marie.png"),
 	preload("res://assets/img/french_politicians/lasalle.png"),
+	preload("res://assets/img/french_politicians/poutou.png"),
+	preload("res://assets/img/french_politicians/melenchon.png"),
+	preload("res://assets/img/french_politicians/jean-marie.png"),
 ]
 
 func get_level_texture(level: int) -> Texture2D:
