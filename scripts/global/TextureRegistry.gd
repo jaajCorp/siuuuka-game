768ecf3805ba3b2eeeb46b31d56622881ef4e23d
GaspardCulis <gaspard@gasdev.fr>
2024-11-14 23:54:49 +0100

    feat(TextureRegistry): Added remote pack fetching code

--- scripts/global/TextureRegistry.gd
+++ scripts/global/TextureRegistry.gd
@@ -1,5 +1,11 @@
 extends Node
 
+var manifest: Dictionary = {}
+
+@onready var http_request: HTTPRequest
+
+const MANIFEST_URL := "https://public.s3web.gasdev.fr/siuuka-game-data/manifest.json"
+const MANIFEST_VERSION := 1
 const POLITICIANS := [
 	preload("res://assets/img/french_politicians/hollande.png"),
 	preload("res://assets/img/french_politicians/sarko.png"),
@@ -14,5 +20,69 @@ const POLITICIANS := [
 	preload("res://assets/img/french_politicians/jean-marie.png"),
 ]
 
+func _ready() -> void:
+	http_request = HTTPRequest.new()
+	add_child(http_request)
+	
+	var pack_list = await fetch_pack_list()
+	print(pack_list)
+	var default_pack_meta = await fetch_pack_metadata(manifest.get("default_pack_id"))
+	print(default_pack_meta)
+
+func fetch_pack_list() -> Dictionary:
+	if manifest.is_empty():
+		await __fetch_manifest()
+		
+	var error = http_request.request(manifest.get("registry_url"))
+	if error != OK:
+		push_error("An error occurred while fetching pack list.")
+		return {}
+	var response = await http_request.request_completed
+	var response_code: int = response[0]
+	if response_code == 200:
+		push_error("Received non 200 response code while fetching registry: ", response_code)
+		return {}
+		
+	var body = response[3]
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	return json.get_data()
+	
+func fetch_pack_metadata(id: String) -> Dictionary:
+	if manifest.is_empty():
+		await __fetch_manifest()
+		
+	var error = http_request.request(manifest.get("packs_url") + id + "/pack.json")
+	if error != OK:
+		push_error("An error occurred while fetching pack list.")
+		return {}
+	var response = await http_request.request_completed
+	var response_code: int = response[0]
+	if response_code == 200:
+		push_error("Received non 200 response code while fetching pack metatata: ", response_code, ", pack_id: ", id)
+		return {}
+		
+	var body = response[3]
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	return json.get_data()
+	
+		
 func get_level_texture(level: int) -> Texture2D:
 	return POLITICIANS[level]
+
+func __fetch_manifest() -> void:
+	var error = http_request.request(MANIFEST_URL)
+	if error != OK:
+		push_error("An error occurred while fetching manifest.")
+		return
+	var response = await http_request.request_completed
+	var response_code: int = response[0]
+	if response_code == 200:
+		push_error("Received non 200 response code while fetching manifest: ", response_code)
+		return
+		
+	var body = response[3]
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	manifest = json.get_data()
