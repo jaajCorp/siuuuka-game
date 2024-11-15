0dff6a5e7dc7cb0478f6570b85eb73ccb000fe34
GaspardCulis <gaspard@gasdev.fr>
2024-11-15 19:27:11 +0100

    chore: Delete now unused ball textures

--- scripts/global/CMS.gd
+++ scripts/global/CMS.gd
@@ -6,19 +6,6 @@ var manifest: Dictionary = {}
 
 const MANIFEST_URL := "https://public.s3web.gasdev.fr/siuuka-game-data/manifest.json"
 const MANIFEST_VERSION := 1
-const POLITICIANS := [
-	preload("res://assets/img/french_politicians/hollande.png"),
-	preload("res://assets/img/french_politicians/sarko.png"),
-	preload("res://assets/img/french_politicians/fillon.png"),
-	preload("res://assets/img/french_politicians/bardella.png"),
-	preload("res://assets/img/french_politicians/marine.png"),
-	preload("res://assets/img/french_politicians/zemmour.png"),
-	preload("res://assets/img/french_politicians/macron.png"),
-	preload("res://assets/img/french_politicians/lasalle.png"),
-	preload("res://assets/img/french_politicians/poutou.png"),
-	preload("res://assets/img/french_politicians/melenchon.png"),
-	preload("res://assets/img/french_politicians/jean-marie.png"),
-]
 
 enum CMSError {
 	OK,
