7941730d3b8914b870a125df5e426dd3c2ce1fab
GaspardCulis <gaspard@gasdev.fr>
2024-11-15 18:20:53 +0100

    feat: Added `Home` scene

---
+++ scripts/logic/home_menu.gd
@@ -0,0 +1,56 @@
+extends Control
+
+@export var status_label: Label
+
+@export var start_button: Button
+@export var store_button: Button
+@export var quit_button: Button
+
+@export var pack: ResourcePack
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_button.disabled = true
+	fetch_remote_data()
+
+func fetch_remote_data():
+	print_status("Fetching status")
+	var manifest: Dictionary = await CMS.fetch_manifest()
+	if check_error(manifest):
+		return
+	
+	print_status("Fetching latest content")
+	var default_pack_id: String = manifest.get("default_pack_id")
+	
+	pack = ResourcePack.new(default_pack_id)
+	pack.connect("load_progress_update", _on_pack_load_progress_update)
+	await pack.loaded
+	
+	print_status("")
+	start_button.disabled = false
+	
+
+func print_status(status: String):
+	status_label.text = status
+	
+func check_error(data: Dictionary):
+	if data.has("error"):
+		print("ERROR: ", data)
+		var error: CMS.CMSError = data.get("error")
+		printerr(error)
+		if error == CMS.CMSError.INTERNET_ERROR:
+			pass
+		elif error == CMS.CMSError.SERVER_ERROR:
+			pass
+		elif error == CMS.CMSError.DATA_INTEGRITY:
+			pass
+		elif error == CMS.CMSError.MANIFEST_VERSION:
+			pass
+		else:
+			printerr("WTF")
+		return true
+	else:
+		return false
+
+func _on_pack_load_progress_update(current: int, total: int):
+	print_status("Fetching latest content (%d/%d)" % [current, total])
