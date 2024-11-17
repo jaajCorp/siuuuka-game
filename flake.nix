19a6cc350c27d2b08064b2e8965fd922806c79ff
GaspardCulis <gaspard@gasdev.fr>
2024-11-17 13:53:46 +0100

    feat(flake): Automatically install Android SDK

--- flake.nix
+++ flake.nix
@@ -6,34 +6,24 @@
   outputs = { self, nixpkgs } @ inputs:
     let
       system = "x86_64-linux"; # whatever your system name is
-      pkgs = import nixpkgs {
-        system = "x86_64-linux"; # whatever your system name is
-        config = {
-          allowUnfree = true;
-          allowUnfreePredicate = _: true;
-          android_sdk.accept_license = true;
-        };
-      };
+      pkgs = import nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = with pkgs; mkShell rec {
-        allowUnfree = true;
-  
         packages = [
           godot_4
-          godot_4-export-templates
           android-tools
           sdkmanager
           openjdk17-bootstrap
         ];
-        buildInputs = [
-          # android-studio-full         
-        ];
 
         shellHook = ''
-          export GODOT4_EXPORT_TEMPLATES=${godot_4-export-templates}
           export JAVA_HOME=${openjdk17-bootstrap}
           export JRE_HOME=${openjdk17-bootstrap}
+          export ANDROID_HOME=~/.local/share/android-sdk
+
+          sdkmanager --sdk_root="$ANDROID_HOME" "platform-tools" "build-tools;33.0.2" "platforms;android-33" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;23.2.8568313"
+          sed -i '1s|#!/bin/bash|#!/usr/bin/env bash|' "$ANDROID_HOME"/build-tools/*/apksigner # FIX: Ugly AF
         '';
       };
     };
