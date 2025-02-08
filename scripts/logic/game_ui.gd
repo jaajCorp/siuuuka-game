a63c20620d661bb225695d42a9c7ea5869080d12
GaspardCulis <gaspard@gasdev.fr>
2025-02-08 09:37:20 +0100

    style(GameUI): Better looking loose popup

--- scripts/logic/game_ui.gd
+++ scripts/logic/game_ui.gd
@@ -5,6 +5,8 @@ extends Control
 
 @onready var game_core := $VBoxContainer/Center/AspectRatioContainer/JarTexture/VBoxContainer/JarInside/GameCore
 @onready var game_over_dialog := $GameOverDialog
+@onready var restart_button := $GameOverDialog/Panel/MarginContainer/VBoxContainer/HBoxContainer/RestartButton
+@onready var quit_button := $GameOverDialog/Panel/MarginContainer/VBoxContainer/HBoxContainer/QuitButton
 @onready var animation_player := $AnimationPlayer
 @onready var best_score_splash := $VBoxContainer/Top/GridContainer/Score/Panel/BestScoreSplash
 @onready var next_ball_preview := $VBoxContainer/Top/GridContainer/Next/MarginContainer/VBoxContainer/Panel/CenterContainer/NextBallPreview
@@ -12,6 +14,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	best_score_splash.visible = false
+	game_over_dialog.visible = false
 	
 	assert(background != null)
 	background.portrait_texture = Global.current_pack.background
@@ -21,8 +24,8 @@ func _ready() -> void:
 	game_core.connect("game_over", _on_game_over)
 	game_core.connect("best_score", _on_best_score)
 	game_core.connect("next_ball_update", _on_next_ball_update)
-	game_over_dialog.connect("confirmed", _on_dialog_confirmed)
-	game_over_dialog.connect("canceled", _on_dialog_canceled)
+	quit_button.connect("pressed", _on_exit_button_pressed)
+	restart_button.connect("pressed", _on_restart_pressed)
 
 
 func _on_game_over():
@@ -37,16 +40,13 @@ func _on_next_ball_update(ball: Ball):
 	next_ball_preview.texture = Global.current_pack.get_level_data(ball.level).texture
 	next_ball_preview.queue_redraw()
 
-func _on_dialog_confirmed():
+func _on_restart_pressed():
+	game_over_dialog.visible = false
 	game_core.reset()
 	
-func _on_dialog_canceled():
+func _on_exit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/Home.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_exit_button_pressed() -> void:
-	_on_dialog_canceled()
