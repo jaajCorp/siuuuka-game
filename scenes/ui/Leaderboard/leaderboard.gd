0629394a08b87b171957fdcd4be631c79da031b9
GaspardCulis <gaspard@gasdev.fr>
2025-02-10 21:31:51 +0100

    fix(Leaderboard): Removed backend specific code

--- scenes/ui/Leaderboard/leaderboard.gd
+++ scenes/ui/Leaderboard/leaderboard.gd
@@ -1,13 +1,10 @@
 extends Control
 
-var client : NakamaClient
-var device_id = OS.get_unique_id()
-
-@onready var socket = Nakama.create_socket_from(client)
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Global.backend.socket.is_connecting_to_host():
+	if not Global.backend.socket:
+		_on_backend_error()
+	elif Global.backend.socket.is_connecting_to_host():
 		Global.backend.socket.connected.connect(_on_backend_ready)
 		Global.backend.socket.connection_error.connect(_on_backend_error)
 	elif Global.backend.socket.is_connected_to_host():
