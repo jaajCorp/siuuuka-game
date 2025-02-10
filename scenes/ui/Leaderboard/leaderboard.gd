9de8da44f76dc6dade4e07e0c1f5e62ad436c363
GaspardCulis <gaspard@gasdev.fr>
2025-02-10 16:04:26 +0100

    refactor(Leaderboard): Moved nakama logic to its own `Backend` global

--- scenes/ui/Leaderboard/leaderboard.gd
+++ scenes/ui/Leaderboard/leaderboard.gd
@@ -7,30 +7,19 @@ var device_id = OS.get_unique_id()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var config := ConfigFile.new()
-	config.load("res://secrets/nakama.cfg")
-	client = Nakama.create_client(
-		config.get_value("client", "server_key"),
-		config.get_value("client", "host"),
-		config.get_value("client", "port"),
-		config.get_value("client", "scheme")
-	)
+	if Global.backend.socket.is_connecting_to_host():
+		Global.backend.socket.connected.connect(_on_backend_ready)
+		Global.backend.socket.connection_error.connect(_on_backend_error)
+	elif Global.backend.socket.is_connected_to_host():
+		_on_backend_ready()
+	else:
+		_on_backend_error()
+
+func _on_backend_ready():
+	print("Ready")
 	
-	var session : NakamaSession = await client.authenticate_device_async(device_id)
-	if session.is_exception():
-		print("An error occurred: %s" % session)
-		return
-	print("Successfully authenticated: %s" % session)
-
-
-	var connected : NakamaAsyncResult = await socket.connect_async(session)
-	if connected.is_exception():
-		print("An error occurred: %s" % connected)
-		return
-	print("Socket connected.")
-
-
-
+func _on_backend_error():
+	print("Errore")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ scripts/global/Backend.gd
@@ -0,0 +1,33 @@
+class_name Backend extends Node
+
+var client : NakamaClient
+var session : NakamaSession 
+var device_id = OS.get_unique_id()
+
+@onready var socket = Nakama.create_socket_from(client)
+
+func _init() -> void:
+	var config := ConfigFile.new()
+	config.load("res://secrets/nakama.cfg")
+	client = Nakama.create_client(
+		config.get_value("client", "server_key"),
+		config.get_value("client", "host"),
+		config.get_value("client", "port"),
+		config.get_value("client", "scheme")
+	)
+	
+	session = await client.authenticate_device_async(device_id)
+	if session.is_exception():
+		print("An error occurred while autheticating to the backend: %s" % session)
+		return
+	print("Successfully authenticated to the backend: %s" % session)
+
+
+	var connected : NakamaAsyncResult = await socket.connect_async(session)
+	if connected.is_exception():
+		print("An error occurred while connecting to backend: %s" % connected)
+		return
+	print("Backend socket connected.")
+
+func is_ok():
+	return socket.is_connected_to_host()
