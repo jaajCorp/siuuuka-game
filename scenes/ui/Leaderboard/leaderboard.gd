54d92886648dca0f8fa446031c5d7305366bba0a
GaspardCulis <gaspard@gasdev.fr>
2025-02-10 21:42:03 +0100

    feat(leaderboard): Got working username edit

--- scenes/ui/Leaderboard/leaderboard.gd
+++ scenes/ui/Leaderboard/leaderboard.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var username_edit := $MarginContainer/VBoxContainer/HBoxContainer/UsernameEdit
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not Global.backend.socket:
@@ -11,13 +13,22 @@ func _ready() -> void:
 		_on_backend_ready()
 	else:
 		_on_backend_error()
+		
+	
 
 func _on_backend_ready():
-	print("Ready")
+	update_username_edit()
 	
 func _on_backend_error():
 	print("Errore")
+	
+func update_username_edit():
+	var account = await Global.backend.client.get_account_async(Global.backend.session)
+	username_edit.text = account.user.username
+	
+func _on_username_edit_submit():
+	var username = username_edit.text
+	await Global.backend.client.update_account_async(Global.backend.session, username)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/Home.tscn")
