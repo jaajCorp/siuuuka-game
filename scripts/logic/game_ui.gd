f70d4342f92267894119e0110f892e95ccea169b
GaspardCulis <gaspard@gasdev.fr>
2025-02-12 16:03:47 +0100

    feat(GameUI): Added save functionnality

--- scripts/logic/game_ui.gd
+++ scripts/logic/game_ui.gd
@@ -3,13 +3,15 @@ extends Control
 @export var background: ResponsiveTextureRect
 
 @onready var game_core := $VBoxContainer/Center/AspectRatioContainer/JarTexture/VBoxContainer/JarInside/GameCore
+@onready var game_serializer := $GameSerializer
 
 @onready var game_over_dialog := $GameOverDialog
 @onready var pause_dialog := $PauseDialog
 
 
 @onready var pause_button := $VBoxContainer/Top/GridContainer/Settings/PauseButton
-@onready var restart_button := $GameOverDialog/Panel/MarginContainer/VBoxContainer/HBoxContainer/RestartButton
+@onready var restart_button := $PauseDialog/Panel/MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/RestartButton
+@onready var game_over_restart_button := $GameOverDialog/Panel/MarginContainer/VBoxContainer/HBoxContainer/RestartButton
 @onready var save_quit_button := $PauseDialog/Panel/MarginContainer/VBoxContainer/VBoxContainer/SaveQuitButton
 
 @onready var animation_player := $AnimationPlayer
@@ -34,8 +36,12 @@ func _ready() -> void:
 	pause_button.connect("pressed", _on_pause_pressed)
 	save_quit_button.connect("pressed", _on_save_quit_button_pressed)
 	restart_button.connect("pressed", _on_restart_pressed)
+	game_over_restart_button.connect("pressed", _on_restart_pressed)
 	
 	get_tree().root.go_back_requested.connect(_on_android_back_pressed)
+	
+	if game_serializer.has_save():
+		game_serializer.restore_game.call_deferred()
 
 
 func _on_game_over(_score: int):
@@ -54,11 +60,15 @@ func _on_pause_pressed():
 	pause_dialog.visible = true
 
 func _on_restart_pressed():
+	pause_dialog.visible = false
 	game_over_dialog.visible = false
 	game_core.reset()
+	game_serializer.reset_save()
 	
 func _on_save_quit_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/Home.tscn")
+	game_serializer.save_game()
+	if get_tree() != null:
+		get_tree().change_scene_to_file("res://scenes/ui/Home.tscn")
 	
 func _on_android_back_pressed() -> void:
 	pause_dialog.visible = not pause_dialog.visible
