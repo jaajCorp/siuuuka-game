bc99715b58b7a1cd720ba520baf9afc5c46d3b3b
GaspardCulis <gaspard@gasdev.fr>
2025-02-18 08:39:36 +0100

    feat(LeaderboardUpdater): Add `besy_score_submitted` signal

--- scripts/components/LeaderboardUpdater.gd
+++ scripts/components/LeaderboardUpdater.gd
@@ -2,11 +2,15 @@ class_name LeaderboardUpdater extends Node
 
 @export var game: GameCore
 
+signal best_score_submitted(leaderboard_name: String)
+
 func _ready() -> void:
 	game.game_over.connect(_on_game_over)
 	
 func _on_game_over(score: int) -> void:
 	var client = Global.backend.client
 	var session = Global.backend.session
-	var all_time_record : NakamaAPI.ApiLeaderboardRecord = await client.write_leaderboard_record_async(session, "all_time_leaderboard", score, 0)
-	var weekly_record : NakamaAPI.ApiLeaderboardRecord = await client.write_leaderboard_record_async(session, "weekly_leaderboard", score, 0)
+	for leaderboard_name in ["all_time_leaderboard", "weekly_leaderboard"]:
+		var record : NakamaAPI.ApiLeaderboardRecord = await client.write_leaderboard_record_async(session, leaderboard_name, score, 0)
+		if record.score == str(score):
+			best_score_submitted.emit(leaderboard_name)
