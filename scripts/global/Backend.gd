9b427866778cc7adc598b51116dcc7edbd3c45a4
GaspardCulis <gaspard@gasdev.fr>
2025-03-13 15:14:30 +0100

    feat(Backend): Add functionnality for web id generation and persist

--- scripts/global/Backend.gd
+++ scripts/global/Backend.gd
@@ -18,7 +18,7 @@ var session : NakamaSession
 var is_healthy := false
 
 var config := ConfigFile.new()
-var device_id = OS.get_unique_id()
+var device_id: String
 
 @onready var refresh_timer: Timer
 
@@ -30,6 +30,23 @@ func _init() -> void:
 		config.get_value("client", "port"),
 		config.get_value("client", "scheme")
 	)
+
+func _ready() -> void:
+	device_id = get_device_id()
+	
+	create_session_and_connect()
+	
+	refresh_timer = Timer.new()
+	refresh_timer.wait_time = 60.0
+	refresh_timer.autostart = false # Started manually after connect
+	refresh_timer.one_shot = false
+	
+	refresh_timer.timeout.connect(_on_session_refresh_timer_timeout)
+	add_child(refresh_timer)
+	
+	self.connected.connect(func (): is_healthy = true; refresh_timer.start())
+	self.session_refreshed.connect(func (): is_healthy = true)
+	self.error.connect(func (_code): is_healthy = false)
 
 func create_session_and_connect():
 	session = await client.authenticate_device_async(device_id)
@@ -56,21 +73,25 @@ func refresh_session():
 		error.emit(Error.SESSION_REFRESH)
 	else:
 		session_refreshed.emit()
-
-func _ready() -> void:
-	create_session_and_connect()
-	
-	refresh_timer = Timer.new()
-	refresh_timer.wait_time = 60.0
-	refresh_timer.autostart = false # Started manually after connect
-	refresh_timer.one_shot = false
-	
-	refresh_timer.timeout.connect(_on_session_refresh_timer_timeout)
-	add_child(refresh_timer)
-	
-	self.connected.connect(func (): is_healthy = true; refresh_timer.start())
-	self.session_refreshed.connect(func (): is_healthy = true)
-	self.error.connect(func (_code): is_healthy = false)
 	
 func _on_session_refresh_timer_timeout() -> void:
 	refresh_session()
+	
+func get_device_id() -> String:
+	var id := OS.get_unique_id()
+	if id == "":
+		if OS.has_feature("web"):
+			var console = JavaScriptBridge.get_interface("console")
+			console.log("Initializing backend")
+			
+			var localStorage = JavaScriptBridge.get_interface("localStorage");
+			var crypto = JavaScriptBridge.get_interface("crypto");
+			
+			id = localStorage.getItem("siuuuka-user-id");
+			if not id:
+				id = "%d-%d-%d-%d" % [randi(), randi(), randi(), randi()]
+				localStorage.setItem("siuuuka-user-id", id)
+		else:
+			push_error("Failed to generate unique device identifier")
+			
+	return id
